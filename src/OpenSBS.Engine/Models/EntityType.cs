83e74414e9c04ed91c1e95b9ea7f29b0cf21ed1e
Matteo G. <matteo.guindani@gmail.com>
2024-08-14 20:57:15 +0200

    Cleaned code

--- src/OpenSBS.Engine/Models/EntityType.cs
+++ src/OpenSBS.Engine/Models/EntityType.cs
@@ -2,14 +2,14 @@
 
 public static class EntityCategory
 {
-    public const string Fighter = "entity-space-ship-fighter";
-    public const string Frigate = "entity-space-ship-frigate";
-    public const string Cruiser = "entity-space-ship-cruiser";
-    public const string BattleCruiser = "entity-space-ship-battlecruiser";
-    public const string Dreadnought = "entity-space-ship-dreadnought";
-    public const string Carrier = "entity-space-ship-carrier";
-    public const string Station = "entity-space-ship-station";
-    public const string Freighter = "entity-spaces-ship-freighter";
-    public const string Container = "entity-space-container";
-    public const string Wreck = "entity-space-wreck";
+    public const string Fighter = "space-ship-fighter";
+    public const string Frigate = "space-ship-frigate";
+    public const string Cruiser = "space-ship-cruiser";
+    public const string BattleCruiser = "space-ship-battlecruiser";
+    public const string Dreadnought = "space-ship-dreadnought";
+    public const string Carrier = "space-ship-carrier";
+    public const string Station = "space-ship-station";
+    public const string Freighter = "spaces-ship-freighter";
+    public const string Container = "space-container";
+    public const string Wreck = "space-wreck";
 }
