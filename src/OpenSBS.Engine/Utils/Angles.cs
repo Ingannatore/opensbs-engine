c0af4a01b5b02d8bba8f82a87fa54adad0b6379d
Matteo G. <matteo.guindani@gmail.com>
2024-08-17 22:16:56 +0200

    Cleaned code

--- src/OpenSBS.Engine/Utils/Angles.cs
+++ src/OpenSBS.Engine/Utils/Angles.cs
@@ -1,18 +1,17 @@
 ﻿using System.Numerics;
 
-namespace OpenSBS.Engine.Utils
+namespace OpenSBS.Engine.Utils;
+
+public static class Angles
 {
-    public static class Angles
+    public static double ToRadians(double value) => value * (Math.PI / 180);
+
+    public static double ToDegrees(double value) => value * (180 / Math.PI);
+
+    public static double GetBearing(Vector3 direction)
     {
-        public static double ToRadians(double value) => value * (Math.PI / 180);
-
-        public static double ToDegrees(double value) => value * (180 / Math.PI);
-
-        public static double GetBearing(Vector3 direction)
-        {
-            var degrees = ToDegrees(Math.Atan2(direction.Z, direction.X));
-            var rotatedDegrees = degrees >= -90 ? degrees - 90 : 270 + degrees;
-            return rotatedDegrees <= 0 ? -rotatedDegrees : 360 - rotatedDegrees;
-        }
+        var degrees = ToDegrees(Math.Atan2(direction.Z, direction.X));
+        var rotatedDegrees = degrees >= -90 ? degrees - 90 : 270 + degrees;
+        return rotatedDegrees <= 0 ? -rotatedDegrees : 360 - rotatedDegrees;
     }
 }
--- src/OpenSBS.Engine/Utils/Vectors.cs
+++ src/OpenSBS.Engine/Utils/Vectors.cs
@@ -1,37 +1,35 @@
-﻿using System;
-using System.Numerics;
+﻿using System.Numerics;
 
-namespace OpenSBS.Engine.Utils
+namespace OpenSBS.Engine.Utils;
+
+public static class Vectors
 {
-    public static class Vectors
+    private const int SectorSize = 50000;
+
+    public static Vector3 Rotate(Vector3 value, double yaw, double pitch, double roll)
     {
-        private const int SectorSize = 50000;
+        var rotationQuaternion = Quaternion.CreateFromYawPitchRoll(
+            (float)yaw,
+            (float)pitch,
+            (float)roll
+        );
 
-        public static Vector3 Rotate(Vector3 value, double yaw, double pitch, double roll)
-        {
-            var rotationQuaternion = Quaternion.CreateFromYawPitchRoll(
-                (float)yaw,
-                (float)pitch,
-                (float)roll
-            );
-
-            return Vector3.Normalize(Vector3.Transform(value, rotationQuaternion));
-        }
-
-        public static Vector3 Move(Vector3 position, Vector3 direction, double value) =>
-            position + direction * (float)value;
-
-        public static Vector2 ToSector(Vector3 position) =>
-            new(
-                (float)Math.Floor((position.X + 25000) / SectorSize),
-                (float)Math.Floor((position.Z + 25000) / SectorSize)
-            );
-
-        public static Vector3 ToSectorCenter(Vector2 sector) =>
-            new(
-                sector.X * SectorSize,
-                0,
-                sector.Y * SectorSize
-            );
+        return Vector3.Normalize(Vector3.Transform(value, rotationQuaternion));
     }
+
+    public static Vector3 Move(Vector3 position, Vector3 direction, double value) =>
+        position + direction * (float)value;
+
+    public static Vector2 ToSector(Vector3 position) =>
+        new(
+            (float)Math.Floor((position.X + 25000) / SectorSize),
+            (float)Math.Floor((position.Z + 25000) / SectorSize)
+        );
+
+    public static Vector3 ToSectorCenter(Vector2 sector) =>
+        new(
+            sector.X * SectorSize,
+            0,
+            sector.Y * SectorSize
+        );
 }
